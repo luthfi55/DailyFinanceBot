b979a0a1cd99dd417dfc33903209b30330e0a2c0
luthfi55 <luthfi12354@gmail.com>
2026-04-28 13:13:43 +0700

    Revert "fix: prisma query engine not found on Vercel"
    This reverts commit a3fe58387f07a05a5b2abe7fd9fcab433dd13951.

--- packages/web/next.config.js
+++ packages/web/next.config.js
@@ -1,18 +1,15 @@
+/** @type {import('next').NextConfig} */
 const path = require("path");
 
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ["@finance/db"],
   outputFileTracingRoot: path.join(__dirname, "../../"),
   serverExternalPackages: ["@prisma/client"],
-
-  experimental: {
-    outputFileTracingIncludes: {
-      "**": [
-        "./node_modules/.prisma/client/**",
-        "./node_modules/@prisma/client/**",
-      ],
-    },
+  outputFileTracingIncludes: {
+    "**": [
+      "node_modules/.pnpm/**/libquery_engine-rhel-openssl-3.0.x.so.node",
+      "node_modules/.pnpm/@prisma+client@*/node_modules/.prisma/client/schema.prisma",
+    ],
   },
 };
 
